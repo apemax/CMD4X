8ed0db551427ff8d9bdc35e9150eac0511f606d6
apemax <apemax18@gmail.com>
2014-04-15 19:56:38 +0100

    Basic fromework for main menu done.

---
+++ global.h
@@ -0,0 +1,30 @@
+/*
+    This file is part of CMD4X.
+
+    CMD4X is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CMD4X is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CMD4X.  If not, see <http://www.gnu.org/licenses/>.
+*/
+// Copyright (C) 2014 Peter Wright
+// author: Peter (apemax) Wright
+// version: 0.0.1
+// CMD4X
+
+#include <string>
+using namespace std;
+
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+string username;
+
+#endif // GLOBAL_H
---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+    This file is part of CMD4X.
+
+    CMD4X is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CMD4X is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CMD4X.  If not, see <http://www.gnu.org/licenses/>.
+*/
+// Copyright (C) 2011 - 2013 Peter Wright
+// author: Peter (apemax) Wright
+// version: 0.0.1
+// CMD4X
+
+
